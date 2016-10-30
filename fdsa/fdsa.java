1430c4983d78fa7356580cb905e04ca3ab740790
nchan6 <norrischan@norriss-air.attlocal.net>
2016-10-30 12:53:15 -0700

    rew

---
+++ fdsa/fdsa.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class fdsa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java. util.*;
+public class fdsa
+{
+    public static void main(String[] args){
+        Scanner x = new Scanner(System.in);
+        for (int i = 5; i >0; i++)
+            System.out.println(i);
+            
+    }
+}
