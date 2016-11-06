a9206ebb7734e0304c9df0269fa3944d9c89c698
nchan6 <norrischan@norriss-air.attlocal.net>
2016-11-06 15:44:38 -0800

    11/5

---
+++ programs/rainfall/Rainfall.java
@@ -0,0 +1,13 @@
+// Norris chan
+// norrischan@my.smccd.edu
+// CIS 254 OL
+// rainfall
+// File Description
+// Lab #6
+// 11/3/16
+public class Rainfall
+{
+    public static void main(String[] args){
+        
+    }
+}
