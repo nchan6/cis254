d08c61d108ad1e36db6d7219ee4bcacd7f1d5d3b
nchan6 <norrischan@Norriss-MacBook-Air.local>
2016-12-09 09:14:44 -0800

    12/9/16

---
+++ programs/mp3/MP3.java
@@ -0,0 +1,24 @@
+// norris chan
+// norrischan@my.smccd.edu
+// CIS 254 OL
+// mp3
+// File Description
+// Assignment #8
+// 12/6/16
+public class MP3
+{
+   private String artist;
+   private String song;
+   private String album;
+   private int trackLength;
+   
+   //constructor
+   public MP3(String artis, String son, String albu, int trackLengt){
+       artist = artis;
+       sond = son;
+       album = albu;
+       trackLength = trackLengh;
+   }
+   
+   
+}
