f0b44fac4a013c01ac8f167339311c1dd5e55f74
nchan6 <norrischan@Norriss-MacBook-Air.local>
2017-01-23 10:29:51 -0800

    1/23/17

---
+++ programs/fdsa/fds.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class fds here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class fds
+{
+    public static void main(String[] args){
+        int limit = 100, num1 = 15, num2 = 40;
+
+if( limit <= limit) {
+    if( num1 == num2) {
+      System.out.print("lemon");
+    }
+    System.out.print("lime");
+} 
+System.out.print("grape");
+    }
+
+
+}
---
+++ programs/magic 8 ball/magic 8 ball/fdsa.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class fdsa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.security.SecureRandom;
+public class fdsa
+{
+    public static void main(String [] args){
+        SecureRandom random = new SecureRandom();
+        int num = random.nextInt(1) * 5;
+        System.out.println(num);
+        
+       
+    }
+}
